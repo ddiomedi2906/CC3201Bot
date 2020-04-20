c2d9a60542a503e30af51cca46d59fb44f45d822
ddiomedi2906 <daniel.diomedi@ug.uchile.cl>
2020-04-20 01:36:34 -0400

    send message on general text channel, minor fix

--- aux_commands/open_close_groups.py
+++ aux_commands/open_close_groups.py
@@ -26,12 +26,18 @@ async def open_group(guild: discord.Guild, group: discord.CategoryChannel):
     if is_closed_group(guild, group):
         GUILD_CONFIG[guild]["CLOSED_GROUPS"].remove(group.name)
     GUILD_CONFIG[guild]["OPEN_GROUPS"].add(group.name)
+    text_channel = hpf.get_lab_text_channel(guild, group.name)
+    if text_channel:
+        await text_channel.send(btm.success_group_open(group))
 
 
 async def close_group(guild: discord.Guild, group: discord.CategoryChannel):
     if is_open_group(guild, group):
         GUILD_CONFIG[guild]["OPEN_GROUPS"].remove(group.name)
     GUILD_CONFIG[guild]["CLOSED_GROUPS"].add(group.name)
+    text_channel = hpf.get_lab_text_channel(guild, group.name)
+    if text_channel:
+        await text_channel.send(btm.success_group_closed(group))
 
 
 async def aux_remove_group(guild: discord.Guild, group: discord.CategoryChannel):
@@ -43,19 +49,25 @@ async def aux_remove_group(guild: discord.Guild, group: discord.CategoryChannel)
 
 async def aux_open_group(ctx, group: discord.CategoryChannel):
     guild = ctx.guild
-    if not hpf.member_in_teaching_team(ctx.author, guild) or group != hpf.existing_member_lab_group(ctx.author):
+    if not (hpf.member_in_teaching_team(ctx.author, guild) or group == hpf.existing_member_lab_group(ctx.author)):
         await ctx.send(btm.error_member_not_part_of_group(ctx.author, group))
     else:
         await open_group(guild, group)
+        general_text_channel = hpf.get_general_text_channel(guild)
+        if general_text_channel:
+            await general_text_channel.send(btm.success_group_open(group))
         print("OPEN_GROUPS", GUILD_CONFIG[guild]["OPEN_GROUPS"])
         print("CLOSED_GROUPS", GUILD_CONFIG[guild]["CLOSED_GROUPS"])
 
 
 async def aux_close_group(ctx, group: discord.CategoryChannel):
     guild = ctx.guild
-    if not hpf.member_in_teaching_team(ctx.author, guild) or group != hpf.existing_member_lab_group(ctx.author):
+    if not (hpf.member_in_teaching_team(ctx.author, guild) or group == hpf.existing_member_lab_group(ctx.author)):
         await ctx.send(btm.error_member_not_part_of_group(ctx.author, group))
     else:
         await close_group(guild, group)
+        general_text_channel = hpf.get_general_text_channel(guild)
+        if general_text_channel:
+            await general_text_channel.send(btm.success_group_closed(group))
         print("OPEN_GROUPS", GUILD_CONFIG[ctx.guild]["OPEN_GROUPS"])
         print("CLOSED_GROUPS", GUILD_CONFIG[ctx.guild]["CLOSED_GROUPS"])
