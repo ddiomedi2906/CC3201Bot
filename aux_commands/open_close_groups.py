1b4800fb09e6a3225475cbb344db2e502c24ca7e
ddiomedi2906 <daniel.diomedi@ug.uchile.cl>
2020-04-20 00:26:13 -0400

    add helper functions

--- aux_commands/open_close_groups.py
+++ aux_commands/open_close_groups.py
@@ -1,15 +1,44 @@
+from typing import List
+
 import discord
 
 from utils.guild_config import GUILD_CONFIG
 import utils.helper_functions as hpf, utils.bot_messages as btm
 
 
-async def is_open_group(guild: discord.Guild, group: discord.CategoryChannel) -> bool:
+def is_open_group(guild: discord.Guild, group: discord.CategoryChannel) -> bool:
     return group.name in GUILD_CONFIG[guild]["OPEN_GROUPS"]
 
 
-async def is_closed_group(guild: discord.Guild, group: discord.CategoryChannel) -> bool:
+def is_closed_group(guild: discord.Guild, group: discord.CategoryChannel) -> bool:
     return group.name in GUILD_CONFIG[guild]["CLOSED_GROUPS"]
+
+
+def all_open_groups(guild: discord.Guild) -> List[discord.CategoryChannel]:
+    return [group for group in hpf.all_existing_lab_groups(guild) if is_open_group(guild, group)]
+
+
+def all_closed_groups(guild: discord.Guild) -> List[discord.CategoryChannel]:
+    return [group for group in hpf.all_existing_lab_groups(guild) if is_closed_group(guild, group)]
+
+
+async def open_group(guild: discord.Guild, group: discord.CategoryChannel):
+    if is_closed_group(guild, group):
+        GUILD_CONFIG[guild]["CLOSED_GROUPS"].remove(group.name)
+    GUILD_CONFIG[guild]["OPEN_GROUPS"].add(group.name)
+
+
+async def close_group(guild: discord.Guild, group: discord.CategoryChannel):
+    if is_open_group(guild, group):
+        GUILD_CONFIG[guild]["OPEN_GROUPS"].remove(group.name)
+    GUILD_CONFIG[guild]["CLOSED_GROUPS"].add(group.name)
+
+
+async def aux_remove_group(guild: discord.Guild, group: discord.CategoryChannel):
+    if is_closed_group(guild, group):
+        GUILD_CONFIG[guild]["CLOSED_GROUPS"].remove(group.name)
+    if is_open_group(guild, group):
+        GUILD_CONFIG[guild]["OPEN_GROUPS"].remove(group.name)
 
 
 async def aux_open_group(ctx, group: discord.CategoryChannel):
@@ -17,9 +46,7 @@ async def aux_open_group(ctx, group: discord.CategoryChannel):
     if not hpf.member_in_teaching_team(ctx.author, guild) or group != hpf.existing_member_lab_group(ctx.author):
         await ctx.send(btm.error_member_not_part_of_group(ctx.author, group))
     else:
-        if is_closed_group(guild, group):
-            GUILD_CONFIG[guild]["CLOSED_GROUPS"].remove(group.name)
-        GUILD_CONFIG[guild]["OPEN_GROUPS"].add(group.name)
+        await open_group(guild, group)
         print("OPEN_GROUPS", GUILD_CONFIG[guild]["OPEN_GROUPS"])
         print("CLOSED_GROUPS", GUILD_CONFIG[guild]["CLOSED_GROUPS"])
 
@@ -29,8 +56,6 @@ async def aux_close_group(ctx, group: discord.CategoryChannel):
     if not hpf.member_in_teaching_team(ctx.author, guild) or group != hpf.existing_member_lab_group(ctx.author):
         await ctx.send(btm.error_member_not_part_of_group(ctx.author, group))
     else:
-        if is_open_group(guild, group):
-            GUILD_CONFIG[ctx.guild]["OPEN_GROUPS"].remove(group.name)
-        GUILD_CONFIG[ctx.guild]["CLOSED_GROUPS"].add(group.name)
+        await close_group(guild, group)
         print("OPEN_GROUPS", GUILD_CONFIG[ctx.guild]["OPEN_GROUPS"])
         print("CLOSED_GROUPS", GUILD_CONFIG[ctx.guild]["CLOSED_GROUPS"])
