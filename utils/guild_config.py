3b386db6b8e2459047228a87200e879fa8c1e173
ddiomedi2906 <daniel.diomedi@ug.uchile.cl>
2020-04-20 16:08:56 -0400

    add init_guild_config, fix serialize GuildDict issue

--- utils/guild_config.py
+++ utils/guild_config.py
@@ -2,7 +2,7 @@ import json
 from asyncio import Lock
 from collections.abc import MutableMapping
 import os
-from typing import List
+from typing import List, Dict
 
 import discord
 
@@ -45,6 +45,7 @@ class GuildConfig:
         self.config_json = config_json
         with open(self.config_json) as inJsonFile:
             data = json.load(inJsonFile)
+        self.backup_data = data
         self.config = {}
         for guild_id, values in data.items():
             self.config[int(guild_id)] = GuildDict(values.items())
@@ -57,6 +58,7 @@ class GuildConfig:
             else:
                 self.config[int(guild_id)]["CLOSED_GROUPS"] = set(self.config[int(guild_id)]["CLOSED_GROUPS"])
 
+    @property
     def guilds(self) -> List[int]:
         return list(self.config.keys())
 
@@ -66,23 +68,49 @@ class GuildConfig:
     def __contains__(self, guild: discord.Guild) -> bool:
         return guild.id in self.config
 
+    def _serialize_guild_dict(self, guild_dict: GuildDict) -> Dict:
+        serialized_dict = {}
+        for key, value in guild_dict.items():
+            serialized_dict[key] = list(value) if type(value) == set else value
+        return serialized_dict
+
+    async def init_guild_config(self, guild: discord.Guild):
+        if guild not in self:
+            self.config[int(guild.id)] = GuildDict()
+            await self.save(guild)
+
     async def save(self, guild) -> bool:
         async with config_lock:
             try:
                 with open(self.config_json) as inJsonFile:
                     data = json.load(inJsonFile)
-                for key, value in self.config[guild.id].items():
-                    data[str(guild.id)][key] = list(value) if type(value) == set else value
-                with open(self.config_json, "w") as outJsonFile:
-                    json.dump(data, outJsonFile, indent=2)
-                return True
             except IOError:
                 return False
+            data[str(guild.id)] = self._serialize_guild_dict(self.config[guild.id])
+            with open(self.config_json, "w") as outJsonFile:
+                try:
+                    json.dump(data, outJsonFile, indent=2)
+                except TypeError:
+                    json.dump(self.backup_data, outJsonFile, indent=2)
+                    return False
+            return True
 
-    async def save_all(self):
+
+    async def save_all(self) -> bool:
         async with config_lock:
             with open(self.config_json, "w") as outJsonFile:
-                json.dump(self.config, outJsonFile, indent=2)
+                serialized_guild_config = {}
+                for guild_id, guild_dict in self.config.items():
+                    serialized_guild_config[guild_id] = self._serialize_guild_dict(guild_dict)
+                try:
+                    json.dump(serialized_guild_config, outJsonFile, indent=2)
+                    self.backup_data = serialized_guild_config
+                    return True
+                except TypeError:
+                    json.dump(self.backup_data, outJsonFile, indent=2)
+            return False
+
+
 
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
