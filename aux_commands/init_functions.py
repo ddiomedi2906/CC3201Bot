66a5c4a7a40f3b68995b0726b487730354ddb5c6
ddiomedi2906 <daniel.diomedi@ug.uchile.cl>
2020-04-20 00:26:59 -0400

    open groups by default when initializing, minor changes

--- aux_commands/init_functions.py
+++ aux_commands/init_functions.py
@@ -1,21 +1,20 @@
 import discord
 
 from aux_commands.create_delete_group import create_new_role
+from aux_commands.open_close_groups import is_closed_group, is_open_group, open_group
+from global_variables import PROFESSOR_ROLE_NAME, HEAD_TA_ROLE_NAME, TA_ROLE_NAME, STUDENT_ROLE_NAME
 from utils.guild_config import GUILD_CONFIG
-from utils.helper_functions import get_nick
+from utils import helper_functions as hpf
 from utils.permission_mask import PMask
 
 
 async def init_guild(guild: discord.Guild):
     if not guild in GUILD_CONFIG:
         return
-    PROFESSOR_ROLE_NAME = GUILD_CONFIG[guild]["PROFESSOR_ROLE_NAME"]
-    HEAD_TA_ROLE_NAME = GUILD_CONFIG[guild]["HEAD_TA_ROLE_NAME"]
-    TA_ROLE_NAME = GUILD_CONFIG[guild]["TA_ROLE_NAME"]
-    STUDENT_ROLE_NAME = GUILD_CONFIG[guild]["STUDENT_ROLE_NAME"]
     print(f'{guild.name}(id: {guild.id})')
-    members = '\n - '.join([get_nick(member) for member in guild.members])
+    members = '\n - '.join([hpf.get_nick(member) for member in guild.members])
     print(f'Guild Members:\n - {members}')
+    # Create or update roles
     all_allow = discord.Permissions.all()
     almost_all = discord.Permissions(PMask.ALL_BUT_ADMIN_AND_GUILD | PMask.STREAM)
     text_and_voice_allow = discord.Permissions(PMask.CHANGE_NICKNAME | PMask.PARTIAL_TEXT | PMask.PARTIAL_VOICE)
@@ -27,4 +26,8 @@ async def init_guild(guild: discord.Guild):
                               hoist=True, mentionable=True)
     await create_new_role(guild, STUDENT_ROLE_NAME, permissions=text_and_voice_allow, colour=discord.Colour.gold(),
                               hoist=True, mentionable=True)
+    # Double check existing lab groups
+    for group in hpf.all_existing_lab_groups(guild):
+        if not (is_open_group(guild, group) or is_closed_group(guild, group)):
+            await open_group(guild, group)
 
