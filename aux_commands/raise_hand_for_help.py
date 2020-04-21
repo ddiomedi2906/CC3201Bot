0fb20e9bf41fa5af06c46ca4d690f3307fb557fb
ddiomedi2906 <daniel.diomedi@ug.uchile.cl>
2020-04-20 20:07:41 -0400

    add help_queue lock, add group into HelpQueue when raising hand

--- aux_commands/raise_hand_for_help.py
+++ aux_commands/raise_hand_for_help.py
@@ -1,3 +1,6 @@
+from asyncio import Lock
+from typing import Union
+
 import discord
 
 from utils.guild_config import GUILD_CONFIG
@@ -9,6 +12,7 @@ from utils import helper_functions as hpf, bot_messages as btm
 ####################################################################
 """
 
+help_queue_lock = Lock()
 
 async def go_for_help(member: discord.Member, lab_group: discord.CategoryChannel, group: int):
     text_channel_name = hpf.get_text_channel_name(group)
@@ -27,22 +31,28 @@ async def go_for_help(member: discord.Member, lab_group: discord.CategoryChannel
 async def aux_raise_hand(ctx):
     member = ctx.author
     existing_lab_group = hpf.existing_member_lab_group(member)
-    general_channel = discord.utils.get(member.guild.channels,
-                                        name=GUILD_CONFIG[ctx.guild]["GENERAL_TEXT_CHANNEL_NAME"])
+    general_text_channel = hpf.get_general_text_channel(ctx.guild)
     if not existing_lab_group:
         await ctx.channel.send(btm.message_member_not_in_group_for_help())
     elif ctx.channel != hpf.existing_member_lab_text_channel(member):
         await ctx.channel.send(btm.message_stay_in_your_seat_error(ctx.author, existing_lab_group.name))
-    elif general_channel:
+    elif general_text_channel:
         online_team = hpf.all_teaching_team_members(ctx.author.guild)
         available_team = [member for member in online_team if not hpf.existing_member_lab_group(member)]
+        help_message = None
         if available_team:
             await ctx.channel.send(btm.message_asking_for_help())
-            await general_channel.send(btm.message_call_for_help(existing_lab_group.name, available_team))
+            help_message = await general_text_channel.send(
+                btm.message_call_for_help(existing_lab_group.name, available_team))
         elif online_team:
             await ctx.channel.send(btm.message_no_one_available_error())
-            await general_channel.send(btm.message_call_for_help(existing_lab_group.name, online_team))
+            help_message = await general_text_channel.send(
+                btm.message_call_for_help(existing_lab_group.name, online_team))
         else:
             await ctx.channel.send(btm.message_no_one_online_error())
+        if help_message:
+            async with help_queue_lock:
+                GUILD_CONFIG.help_queue(ctx.guild).add(group=hpf.get_lab_group_number(existing_lab_group.name),
+                                                       message_id=help_message.id)
     else:
         await ctx.channel.send(btm.message_can_not_get_help_error())
